1892f26fa75f29996a1aa473d8f078fb3a6c5243
aitxiaogang <aitxiaogang@163.com>
2017-05-14 21:24:52 +0800

    添加依赖

--- src/test/java/com/xiaogang/ssh/test/TestJunit.java
+++ src/test/java/com/xiaogang/ssh/test/TestJunit.java
@@ -1,12 +1,30 @@
 package com.xiaogang.ssh.test;
 
-import static org.junit.Assert.*;
+import java.io.Serializable;
+
+import javax.annotation.Resource;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.xiaogang.ssh.entity.UserInfo;
+import com.xiaogang.ssh.service.UserInfoService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
 public class TestJunit {
+	
+	@Resource
+	private UserInfoService userInfoService;
+	
 	@Test
 	public void testName() throws Exception {
-		System.out.println("TestJunit.testName()");
+		System.out.println(userInfoService);
+		UserInfo userInfo = new UserInfo();
+		userInfo.setUsername("xiaogang");
+		Serializable serializable = userInfoService.addUserInfo(userInfo);
+		System.out.println(serializable);
 	}
 }
