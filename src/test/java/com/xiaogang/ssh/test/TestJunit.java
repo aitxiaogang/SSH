1114b9da48ed7167358ef73ce9046bcfc28e1021
aitxiaogang <aitxiaogang@163.com>
2017-05-14 10:15:32 +0800

    add conf

---
+++ src/test/java/com/xiaogang/ssh/test/TestJunit.java
@@ -0,0 +1,12 @@
+package com.xiaogang.ssh.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestJunit {
+	@Test
+	public void testName() throws Exception {
+		System.out.println("TestJunit.testName()");
+	}
+}
